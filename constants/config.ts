dd49fabdcda820b0cf02a939bca4a7fa6b984566
duyenduyen8301 <james.nguyen@amela.vn>
2026-01-21 14:16:10 +0700

    Update environment variables and modify NotFound component
    - Added NEXT_PUBLIC_API_DOMAIN to the .env file for additional configuration.
    - Updated the NotFound component by removing the variant prop from the Button for consistency in styling.
    - Included API_DOMAIN in the config constants for better access to the new environment variable.

--- constants/config.ts
+++ constants/config.ts
@@ -3,6 +3,7 @@ const configs = {
   BACKLOG_API_KEY: `${process.env.NEXT_PUBLIC_BACKLOG_API_KEY}`,
   BACKLOG_PROJECT_ID_OR_KEY: `${process.env.NEXT_PUBLIC_BACKLOG_PROJECT_KEY}`,
   BACKLOG_PROJECT_ID: `${process.env.NEXT_PUBLIC_BACKLOG_PROJECT_ID}`,
+  API_DOMAIN: `${process.env.NEXT_PUBLIC_API_DOMAIN}`,
 };
 
 export default configs;
